41a97a4741abf6f452b382aec76abc2df4e5d944
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-11 19:53:12 +0800

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num,num1,niilber;
+	cout<<"too oruul:";
+	cin>>num;
+	cout<<"tanii oruulsan too "<<num;
+	cout<<"Tanii oruulsan 2 dahi too";
+	cin>>num1;
+	
+	niilber=num+num1;
+	
+	cout<<"tanii oruulsan 2toomii niilber "<<niilber;
+	
+	return 0;
+}
---
+++ hicheel 1..cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello Dulguun"<<endl;
+	cout<<"Hello Dulguun";
+	return 0;
+}
