f80a06b48110952aa7a250e11a7848ccbfe55e91
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-12 21:41:48 +0800

    Add files via upload

---
+++ daalgavar 5.11(bodlogo).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d;
+	cout<<"Tegsh untsugtiin urt: ";
+	cin>>a;
+	cout<<"tegsh untsugtiin urgun: ";
+	cin>>b;
+	c=(a+b)*2;
+	d=a*b;
+	cout<<"TEGSH UNTSUGTIIN PRIMETERIIN URT:"<<c<<endl;
+	cout<<"TEGSH UNTSUGTIIN TALBAI: "<<d;
+	return 0;
+	
+}
