a1107c9a8b5188cb6000e6fd2806dda235f3d735
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-12 21:41:02 +0800

    Add files via upload

---
+++ daalgavar 5.11(neg toogoor).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d,e,f;
+	cout<<"ehnii too: ";
+	cin>>a;
+	cout<<"hoer dahi too: ";
+	cin>>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	cout<<"HOER TOONII NIILBER: "<<c<<endl;
+	cout<<"HOER TOONII YALGAWAR: "<<d<<endl;
+	cout<<"HOER TOONII VRJWER: "<<e<<endl;
+	cout<<"HOER TOONII NOOGDWOR: "<<f<<endl;
+	return 0;
+}
