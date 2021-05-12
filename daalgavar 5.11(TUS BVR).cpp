6f595b6892cca29047fea2ac1c2daa9f36e45d52
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-12 21:20:54 +0800

    Add files via upload

---
+++ daalgavar 5.11(TUS BVR).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+	float a,b,c,d,e,f,g,h,i,j,k,l;  // zarlaj bna
+	cout<<"negdvgeer nemegdehvvn :";
+	cin>>a;
+	cout<<"hoerdugaar nemegdehvvn: ";
+	cin>>b;
+	c=a+b;
+	cout<<"HOER TOONII NIILBER: "<<c<<endl; // niilber 
+	cout<<"vrjigdehvvn: ";
+	cin>>d;
+	cout<<"vrjver: ";
+	cin>>e;
+	f=e*d;
+	cout<<"HOER TOONII VRJWER: "<<f<<endl; // VRJWER
+	cout<<"huwaagdagch:";
+	cin>>g;
+	cout<<"huwaagch:";
+	cin>>h;
+	i=g/h;
+	cout<<"HOER TOONII NOOGDWOR: "<<i<<endl; // noogdwor
+	cout<<"hasagdagch: ";
+	cin>>j;
+	cout<<"hasagch: ";
+	cin>>k;	
+	l=j-k;
+	cout<<"HOER TOONII YALGAWAR: "<<l; //yalgawar
+	
+	
+	return 0;
+	
+}
