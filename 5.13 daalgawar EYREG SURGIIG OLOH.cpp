45477e2b5d35ab91a4ab2ffdb18f40623c47ce3b
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-13 21:55:19 +0800

    Add files via upload

---
+++ 5.13 daalgawar EYREG SURGIIG OLOH.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a1;
+	cout<<"Ta toogoo oruulna uu: ";
+	cin>>a1;
+	if(a1<0)
+	{
+		cout<<"tanii too surug too baina."<<endl;	
+	}
+	else
+	{
+		cout<<"Tanii too eyreg too baina."<<endl;
+	}	
+	return 0;
+}
