89298c9442ee261d562c3bdccdce182e5760c467
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-13 21:47:59 +0800

    Add files via upload

---
+++ 5.13 daalgawar TEGSH SONDGOIG OLOH.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int a1,a2;
+	cout<<"Ta toogoo oruulna uu: ";
+	cin>>a1;
+	a2=a1%2;
+	if(a2>0) 
+	{
+		cout<<"tanii too SONDGOI too baina"<<endl;
+	}
+	else
+	{
+	
+		cout<<"tanii too TEGSH too baina"<<endl;
+	}
+	
+	return 0;
+		
+}
